0bc2ff3a67e1e9dd39635deb1b5c456d9b804200
sumeet <sumeetsaini621@gmail.com>
2025-07-17 21:23:08 +0100

    add some zsh plugins

--- dot-config/zsh/conf/plugins.zsh
+++ dot-config/zsh/conf/plugins.zsh
@@ -1,1 +1,7 @@
+PLUGIN_DIR=/Users/sumeet/.config/zsh/conf/plugins
+
 eval "$(atuin init zsh)"
+
+source "$PLUGIN_DIR/zsh-autosuggestions/zsh-autosuggestions.zsh"
+source "$PLUGIN_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+
