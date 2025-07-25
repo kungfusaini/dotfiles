642538b3e19053454662b1a3fcb672efc6ab14df
sumeet <sumeetsaini621@gmail.com>
2025-07-25 17:52:53 +0100

    add calibre and raycast search plugs

--- dot-config/nix/flake.nix
+++ dot-config/nix/flake.nix
@@ -66,6 +66,7 @@
 
 		casks = [
 			"activitywatch"
+			"calibre"
 			"itsycal" # this didn't work in nix as it needs to go into the application folder
 			"karabiner-elements" # this didn't work with nix as it didn't ask for permissions correctly
 			"nordvpn"
