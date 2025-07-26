ad6c9226751c9a7b1d6cd7116bdd7fd0297e8acb
sumeet <sumeetsaini621@gmail.com>
2025-07-26 13:05:31 +0100

    cleanup keyboard shortcuts

--- dot-config/zsh/conf/plugins.zsh
+++ dot-config/zsh/conf/plugins.zsh
@@ -3,7 +3,6 @@ PLUGIN_DIR=/Users/sumeet/.config/zsh/conf/plugins
 eval "$(atuin init zsh)"
 eval "$(zoxide init zsh)"
 
-source "$PLUGIN_DIR/fzf-git.sh/fzf-git.sh"
 source "$PLUGIN_DIR/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 source "$PLUGIN_DIR/zsh-vi-mode/zsh-vi-mode.plugin.zsh"
 
@@ -11,4 +10,6 @@ source "$PLUGIN_DIR/zsh-autosuggestions/zsh-autosuggestions.zsh"
 bindkey '^ ' autosuggest-accept
 
 source <(fzf --zsh)
+source "$PLUGIN_DIR/fzf-git.sh/fzf-git.sh"
+bindkey -M vicmd -r "^G" # Unbind list-expand in command mode (allowed in insert mode)
 
