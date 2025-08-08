297f66468b2e81cdf16eb8b84a3a74d3268f6ad8
sumeet <sumeetsaini621@gmail.com>
2025-08-08 21:31:35 +0100

    adds indent plugin

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -23,6 +23,6 @@ vim.api.nvim_create_autocmd("BufWinLeave", {
 vim.api.nvim_create_autocmd("BufWinEnter", {
   pattern = "*",
   callback = function()
-    vim.cmd("silent loadview")
+    vim.cmd("silent! loadview")
   end,
 })
