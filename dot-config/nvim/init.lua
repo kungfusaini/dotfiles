2e800020f5a78f8191b60240870dcbcdab6ff9aa
sumeet <sumeetsaini621@gmail.com>
2025-08-08 20:46:13 +0100

    finish initial status bar

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -5,7 +5,24 @@ vim.cmd.colorscheme("gruvbox")
 vim.opt.clipboard = "unnamedplus"
 vim.opt["tabstop"] = 4
 vim.opt["shiftwidth"] = 4
+vim.opt.foldlevel = 99
 
 vim.wo.relativenumber = true
+vim.opt.foldenable = true
+vim.opt.foldmethod = "syntax"
 
 vim.o.statuscolumn = "%!v:lua.require('config.statuscolumn').myStatuscolumn()";
+
+vim.api.nvim_create_autocmd("BufWinLeave", {
+  pattern = "*",
+  callback = function()
+      vim.cmd("mkview")
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  pattern = "*",
+  callback = function()
+    vim.cmd("silent loadview")
+  end,
+})
--- dot-config/nvim/lua/config/statuscolumn.lua
+++ dot-config/nvim/lua/config/statuscolumn.lua
@@ -27,12 +27,42 @@ statuscolumn.number = function()
   return tostring(num)
 end
 
+
+statuscolumn.folds = function ()
+  local foldlevel = vim.fn.foldlevel(vim.v.lnum);
+  local foldlevel_before = vim.fn.foldlevel((vim.v.lnum - 1) >= 1 and vim.v.lnum - 1 or 1);
+  local foldlevel_after = vim.fn.foldlevel((vim.v.lnum + 1) <= vim.fn.line("$") and (vim.v.lnum + 1) or vim.fn.line("$"));
+
+
+  local foldclosed = vim.fn.foldclosed(vim.v.lnum);
+  -- Line has nothing to do with folds so we will skip it
+  if foldlevel == 0 then
+    return " ";
+  end
+  -- Line is a closed fold(I know second condition feels unnecessary but I will still add it)
+  if foldclosed ~= -1 and foldclosed == vim.v.lnum then
+    return "▶";
+  end
+  if foldlevel > foldlevel_before then
+    return "▽"
+  end
+
+  -- The line is the last line in the fold
+  if foldlevel > foldlevel_after then
+    return "╰";
+  end
+
+  -- Line is in the middle of an open fold
+  return "╎";
+end
+
 statuscolumn.myStatuscolumn = function ()
   local text = "";
 
   statuscolumn.setHl();
 
   text = table.concat({
+	  statuscolumn.folds(),
 	  statuscolumn.number({ mode = "hybrid"}),
 	  statuscolumn.border(),
 	})
