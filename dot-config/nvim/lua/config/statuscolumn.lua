2765877c35be9f738bd3df894436b9bd90707edd
sumeet <sumeetsaini621@gmail.com>
2025-08-30 13:58:59 -0400

    updates colors

--- dot-config/nvim/lua/config/statuscolumn.lua
+++ dot-config/nvim/lua/config/statuscolumn.lua
@@ -1,31 +1,35 @@
 local statuscolumn = {};
 
-
 statuscolumn.setHl = function()
   -- Gradient from colordesiner.io
-  local colors = { "#50aac3", "#4ea6ae", "#55a199", "#609a86", "#6b9377",
-    "#748b6d", "#7b8366", "#7f7c63", "#7f7563", "#7d6f64",
+  local colors = {
+    "#f96c00",
+    "#d5761d",
+    "#b57934",
+    "#98784b",
+    "#7e7463",
   };
 
   for i, color in ipairs(colors) do
     vim.api.nvim_set_hl(0, "Gradient_" .. i, { fg = color });
   end
 
-  vim.api.nvim_set_hl(0, "LspErrorIcon", { fg = "#ff0000" }) -- Red for errors
-  vim.api.nvim_set_hl(0, "LspWarnIcon", { fg = "#ffbf00" })  -- Orange for warnings
-  vim.api.nvim_set_hl(0, "LspInfoIcon", { fg = "#00bfff" })  -- Blue for info
-  vim.api.nvim_set_hl(0, "LspHintIcon", { fg = "#808080" })  -- Gray for hints
-  vim.api.nvim_set_hl(0, "GitSignsAdd", { fg = "#39a544" })
-  vim.api.nvim_set_hl(0, "GitSignsChange", { fg = "#fabe30" })
-  vim.api.nvim_set_hl(0, "GitSignsDelete", { fg = "#e64141" })
+  vim.api.nvim_set_hl(0, "CurrentLineNum", { fg = "#f96c00" }) -- Orange
+  vim.api.nvim_set_hl(0, "LspErrorIcon", { fg = "#FC4934" })   -- Red
+  vim.api.nvim_set_hl(0, "LspWarnIcon", { fg = "#ffbf00" })    -- Orange
+  vim.api.nvim_set_hl(0, "LspInfoIcon", { fg = "#83A598" })    -- Blue
+  vim.api.nvim_set_hl(0, "LspHintIcon", { fg = "#B8BB27" })    -- Green
+  vim.api.nvim_set_hl(0, "GitSignsAdd", { fg = "#B8BB27" })    -- Green
+  vim.api.nvim_set_hl(0, "GitSignsChange", { fg = "#FABE30" }) -- Yellow
+  vim.api.nvim_set_hl(0, "GitSignsDelete", { fg = "#FC4934" }) -- Red
 end
 
 statuscolumn.border = function()
   -- lua tables start at 1 but relnum starts at 0, so we add 1 to it to get the highlight group
-  if vim.v.relnum < 9 then
+  if vim.v.relnum < 4 then
     return "%#Gradient_" .. (vim.v.relnum + 1) .. "#┃ ";
   else
-    return "%#Gradient_10#┃";
+    return "%#Gradient_5#┃";
   end
 end
 
@@ -41,23 +45,28 @@ statuscolumn.git_hl = function()
       return "%#GitSignsDelete#"
     end
   end
-  return "%*" -- Reset to default highlight
+  return "" -- Just return empty string, don't reset
 end
 
 statuscolumn.number = function()
-  -- If relative number is 0 (current line), show absolute line number instead
-  local git_hl = statuscolumn.git_hl()
   local num = (vim.v.relnum == 0) and vim.v.lnum or vim.v.relnum
   local padding_width = 4
-  return git_hl .. string.format("%" .. padding_width .. "d", tostring(num))
-end
+  local num_str = tostring(num)
+  local padding = string.rep(" ", padding_width - #num_str)
 
+  if vim.v.relnum == 0 then
+    local git_hl = statuscolumn.git_hl()
+    -- Apply git highlight to padding, special highlight to number
+    return git_hl .. padding .. "%#CurrentLineNum#" .. num_str
+  else
+    return statuscolumn.git_hl() .. padding .. num_str
+  end
+end
 
 statuscolumn.folds = function()
   local foldlevel = vim.fn.foldlevel(vim.v.lnum);
   local foldlevel_before = vim.fn.foldlevel((vim.v.lnum - 1) >= 1 and vim.v.lnum - 1 or 1);
   local foldlevel_after = vim.fn.foldlevel((vim.v.lnum + 1) <= vim.fn.line("$") and (vim.v.lnum + 1) or vim.fn.line("$"));
-
 
   local foldclosed = vim.fn.foldclosed(vim.v.lnum);
   -- Line has nothing to do with folds so we will skip it
@@ -95,26 +104,23 @@ statuscolumn.diag = function()
 
     -- Then, use the highest severity level to determine the symbol to display
     if highest_severity <= vim.diagnostic.severity.ERROR then
-      symbol = "%#LspErrorIcon#%*"
+      symbol = "%#LspErrorIcon#%*"
     elseif highest_severity <= vim.diagnostic.severity.WARN then
-      symbol = "%#LspWarnIcon#%*!"
+      symbol = "%#LspWarnIcon#!%*"
     elseif highest_severity <= vim.diagnostic.severity.INFO then
-      symbol = "%#LspInfoIcon#%*i"
+      symbol = "%#LspInfoIcon#i%*"
     elseif highest_severity <= vim.diagnostic.severity.HINT then
-      symbol = "%#LspHintIcon#%*"
+      symbol = "%#LspHintIcon#%*"
     end
   end
 
   return symbol;
 end
 
-
 statuscolumn.myStatuscolumn = function()
-  local text = "";
-
   statuscolumn.setHl();
 
-  text = table.concat({
+  local text = table.concat({
     statuscolumn.diag(),
     statuscolumn.number(),
     statuscolumn.folds(),
