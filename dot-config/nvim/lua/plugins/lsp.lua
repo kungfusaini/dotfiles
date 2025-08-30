0b393c71f14e90a3f8197dd7218063a9f022a638
sumeet <sumeetsaini621@gmail.com>
2025-08-30 07:18:29 -0400

    auto format code on save

--- dot-config/nvim/lua/plugins/lsp.lua
+++ dot-config/nvim/lua/plugins/lsp.lua
@@ -1,26 +1,58 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
+  {
+    "neovim/nvim-lspconfig",
 
-		dependencies = {
-			{
+    dependencies = {
+      {
 
-    "folke/lazydev.nvim",
-    ft = "lua", -- only load on lua files
-    opts = {
-      library = {
-        -- See the configuration section for more details
-        -- Load luvit types when the `vim.uv` word is found
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+          library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
       },
     },
-  },
-		};
-		config = function()
-			require("lspconfig").basedpyright.setup{}
-			require("lspconfig").clangd.setup{}
-			require("lspconfig").lua_ls.setup{}
-			require("lspconfig").nil_ls.setup{}
-		end,
-	}
+    config = function()
+      require("lspconfig").basedpyright.setup {}
+      require("lspconfig").clangd.setup {}
+      require("lspconfig").lua_ls.setup {}
+      require("lspconfig").nil_ls.setup {}
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('my.lsp', {}),
+        callback = function(args)
+          local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+          if not client then return end
+
+          -- if client:supports_method('textDocument/implementation') then
+          -- Create a keymap for vim.lsp.buf.implementation ...
+          -- end
+
+          -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
+          -- if client:supports_method('textDocument/completion') then
+          -- Optional: trigger autocompletion on EVERY keypress. May be slow!
+          -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+          -- client.server_capabilities.completionProvider.triggerCharacters = chars
+
+          -- vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+          -- end
+
+          -- Autoformat on save
+          if client:supports_method('textDocument/formatting') then
+            vim.api.nvim_create_autocmd('BufWritePre', {
+              group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
+              buffer = args.buf,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+              end,
+            })
+          end
+        end,
+      })
+    end,
+  }
 }
