a6c1ca29cce06ad6cc24b03cb0c56d59e8ba52e2
sumeet <sumeetsaini621@gmail.com>
2025-12-18 21:23:33 +0000

    adds last note grabber yd

--- dot-config/nvim/lua/config/functions.lua
+++ dot-config/nvim/lua/config/functions.lua
@@ -13,6 +13,24 @@ M.open_todays_note = function()
   vim.cmd("edit " .. file)
 end
 
+M.open_latest_previous_note = function()
+  local journal_dir = vim.fn.expand("~/codex/Journal/")
+  local today = os.date("%Y-%m-%d")
+  
+  local files = vim.fn.glob(journal_dir .. "*.md", false, true)
+  table.sort(files, function(a, b) return a > b end)
+  
+  for _, file in ipairs(files) do
+    local filename = vim.fn.fnamemodify(file, ":t:r")
+    if filename ~= today then
+      vim.cmd("edit " .. file)
+      return
+    end
+  end
+  
+  vim.notify("No previous notes found", vim.log.levels.WARN)
+end
+
 M.toggle_zoom = function()
   local current_win = vim.api.nvim_get_current_win()
 
@@ -31,6 +49,7 @@ end
 M.setup = function()
   vim.api.nvim_create_user_command("Today", M.open_todays_note, {})
   vim.keymap.set("n", "<leader>td", M.open_todays_note, { desc = "Open today's note" })
+  vim.keymap.set("n", "<leader>yd", M.open_latest_previous_note, { desc = "Open latest previous note" })
 
   vim.keymap.set('n', '<leader><CR>', M.toggle_zoom, { desc = 'Toggle zoom window' })
 end
