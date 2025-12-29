708078f9bd061fedb69290bd421a5d610a3c9e6c
sumeet <sumeetsaini621@gmail.com>
2025-12-29 18:16:49 +0000

    adds auto preferences to sioyek

--- dot-config/nvim/lua/plugins/vimtex.lua
+++ dot-config/nvim/lua/plugins/vimtex.lua
@@ -4,4 +4,12 @@ return {
   init = function()
     vim.g.vimtex_view_method = "sioyek"
   end,
+
+  config = function()
+    -- Doesn't work :(
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'VimtexEventViewReverse',
+      command = "call b:vimtex.viewer.xdo_focus_vim()"
+    })
+  end,
 }
